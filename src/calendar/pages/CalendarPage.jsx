775b3e40c4bb9f3917363a0bb950dc250400964d
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-22 08:39:40 +0100

    react big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,55 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
 import { Navbar } from "../"
+
+import { localizer } from '../../helpers';
+
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Alvaro'
+    }
+}]
 
 
 export const CalendarPage = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundcolor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+
+    }
+
     return (
         <>
             <Navbar />
+
+            < Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                eventPropGetter={eventStyleGetter}
+            />
+
+
         </>
     )
 }
