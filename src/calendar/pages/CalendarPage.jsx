37a853736981eba8eb43a46d94c6c074b963210e
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-23 07:40:42 +0100

    load event

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -12,7 +12,7 @@ import { useUiStore, useCalendarStore } from '../../hooks';
 export const CalendarPage = () => {
 
     const { openDateModal } = useUiStore();
-    const { events } = useCalendarStore();
+    const { events, setActiveEvent } = useCalendarStore();
 
     const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week')
 
@@ -35,7 +35,7 @@ export const CalendarPage = () => {
     };
 
     const onSelect = (event) => {
-        console.log({ click: event })
+        setActiveEvent(event)
     };
 
     const onViewChanged = (event) => {
