10e924ffc523ae92ded74099bc040f8bfaef7b2c
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-30 22:35:11 +0100

    maintain login

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -43,6 +43,22 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async () => {
+        const token = localStorage.getItem('token');
+        if (!token) return dispatch(onLogout());
+
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
+
+        } catch (error) {
+            localStorage.clear();
+            dispatch(onLogout());
+        }
+    }
+
     return {
         // *Props
         status,
@@ -51,7 +67,8 @@ export const useAuthStore = () => {
 
         // *Methods
         startLogin,
-        startRegister
+        startRegister,
+        checkAuthToken
     }
 }
 
